122047e42aff4b7d80efbff4901d32a77244c397
jackdev <jackdev@jackproject.org>
2019-06-27 19:50:13 +0900

    Remove debug from launcher

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,4 @@
 
 cd /opt/jack
 
-exec ./bin/jackd -datadir=/var/lib/coindata -debug -server -rest=1 -rpcbind=0.0.0.0 -rpcuser=user -rpcpassword=password -rpcallowip=0.0.0.0/0 -rpcport=7777
+exec ./bin/jackd -datadir=/var/lib/coindata -server -rest=1 -rpcbind=0.0.0.0 -rpcuser=user -rpcpassword=password -rpcallowip=0.0.0.0/0 -rpcport=7777 $*
