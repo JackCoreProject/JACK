6c6fd73a5e635db9b6ba2c9d6ef1933c24280358
jackdev <jackdev@jackproject.org>
2019-06-28 16:38:18 +0900

    update makeseeds.py

--- contrib/seeds/makeseeds.py
+++ contrib/seeds/makeseeds.py
@@ -26,7 +26,7 @@ import collections
 PATTERN_IPV4 = re.compile(r"^((\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})):(\d+)$")
 PATTERN_IPV6 = re.compile(r"^\[([0-9a-z:]+)\]:(\d+)$")
 PATTERN_ONION = re.compile(r"^([abcdefghijklmnopqrstuvwxyz234567]{16}\.onion):(\d+)$")
-PATTERN_AGENT = re.compile(r"^(/JACKCore:2.2.(0|1|99)/)$")
+PATTERN_AGENT = re.compile(r"^(/JACK Core:[0-9.]/)$")
 
 def parseline(line):
     sline = line.split()
@@ -151,7 +151,7 @@ def main():
     # Require at least 50% 30-day uptime.
     ips = [ip for ip in ips if ip['uptime'] > 50]
     # Require a known and recent user agent.
-    ips = [ip for ip in ips if PATTERN_AGENT.match(re.sub(' ', '-', ip['agent']))]
+    #ips = [ip for ip in ips if PATTERN_AGENT.match(re.sub(' ', '-', ip['agent']))]
     # Sort by availability (and use last success as tie breaker)
     ips.sort(key=lambda x: (x['uptime'], x['lastsuccess'], x['ip']), reverse=True)
     # Filter out hosts with multiple bitcoin ports, these are likely abusive
