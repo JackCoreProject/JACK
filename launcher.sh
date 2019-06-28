6a832163855dd67721151ccb72992a0f40285666
jackdev <jackdev@jackproject.org>
2019-06-28 14:19:25 +0900

    Fix stupid bug

--- launcher.sh
+++ launcher.sh
@@ -4,15 +4,15 @@ cd /opt/jack
 
 JACKOPTS="-datadir=/var/lib/coindata -server -rest=1 -rpcbind=0.0.0.0 -rpcuser=user -rpcpassword=password -rpcallowip=0.0.0.0/0 -rpcport=7777"
 
-if [ "MINING" = "true" ]; then
+if [ "$MINING" = "true" ]; then
     JACKOPTS="$JACKOPTS -gen=1 -genproclimit=-1"
 fi
 
-if [ "STAKING" = "true" ]; then
+if [ "$STAKING" = "true" ]; then
     JACKOPTS="$JACKOPTS -staking=1 -zjackstake=1 -jackstake=1"
 fi
 
-if [ "MASTERNODE" = "true" ]; then
+if [ "$MASTERNODE" = "true" ]; then
     JACKOPTS="$JACKOPTS -masternode=1"
 fi
 
