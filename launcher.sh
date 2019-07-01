8b0c3fa60ea16b52ec034cd686cf4c7351c060d8
jackdev <jackdev@jackproject.org>
2019-07-01 15:30:37 +0900

    Configurable rpcport

--- launcher.sh
+++ launcher.sh
@@ -2,7 +2,7 @@
 
 cd /opt/jack
 
-JACKOPTS="-datadir=/var/lib/coindata -daemon=0 -server -rest=1 -rpcbind=0.0.0.0 -rpcuser=user -rpcpassword=password -rpcallowip=0.0.0.0/0 -rpcport=7777"
+JACKOPTS="-datadir=/var/lib/coindata -daemon=0"
 
 if [ "$MINING" = "true" ]; then
     JACKOPTS="$JACKOPTS -gen=1 -genproclimit=-1"
@@ -14,6 +14,22 @@ fi
 
 if [ "$MASTERNODE" = "true" ]; then
     JACKOPTS="$JACKOPTS -masternode=1"
+fi
+
+if [ "$RPCSERVER" = "true" ]; then
+    JACKOPTS="$JACKOPTS -rest=1 -server -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0"
+
+    if [ "$RPCUSER" != "" ]; then
+        JACKOPTS="$JACKOPTS -rpcuser=$RPCUSER"
+    fi
+
+    if [ "$RPCPASS" != "" ]; then
+        JACKOPTS="$JACKOPTS -rpcpassword=$RPCPASS"
+    fi
+
+    if [ "$RPCPORT" != "" ]; then
+        JACKOPTS="$JACKOPTS -rpcport=$RPCPORT"
+    fi
 fi
 
 exec ./bin/jackd $JACKOPTS
